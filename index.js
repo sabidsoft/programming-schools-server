c0de2ce8011250210efb69597fd7010ef81a7e02
Sabid Hasan <sabid.soft@gmail.com>
2022-10-26 23:34:06 +0600

    an api added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 const courses = require('./data/languages.json')
-
+const coursesDetails = require('./data/details.json')
 
 app.use(cors())
 
@@ -16,14 +16,10 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
-
+app.get('/courses/:id', (req, res) => {
+    res.send(coursesDetails.find(course => course.course_id === req.params.id))
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
-
-//const coursesDetails = require('./data/details.json')
-
-// app.get('/courses/:id', (req, res) => {
-//     res.send(coursesDetails.find(course => course.course_id === req.params.id))
-// })
